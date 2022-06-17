86703e17a275d0543b1f860f64bca279d6cade89
Taeyong Lee <d104601@hotmail.com>
2022-06-17 04:40:55 -0400

    finished category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -50,7 +50,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const catData = await Category.update(req.body,{
+    const catData = await Category.update(req.body, {
       where: {
         id: req.params.id
       }
@@ -68,6 +68,19 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const catData = await Category.destory({
+      where: {
+        id: req.params.id
+      }});
+    if(!catData){
+      res.status(404).json({ message: "No category in this id"});
+      return;
+    }
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
